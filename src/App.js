9b6bda6b2ac030520c55e0bbd23ef6d4e775a2ca
gungun <gunji.mamoru@gmail.com>
2025-02-28 13:34:43 +0900

    課題１実装

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          テスト
+          課題１
         </p>
         <a
           className="App-link"
